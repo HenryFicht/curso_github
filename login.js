7e6c856dee13857e5229c77075a69335bacb1bdb
HenryDev <henrique.ficht@gmail.com>
2025-02-27 21:39:39 -0300

    Arrumando o bug do sistema de login

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 // Trabalhando no login
 
 // Finalizando o sistema de login
-function login (nome,email){
-    return'Bem vindo';
-}
 
 
 
-// Sistema de login completo
-// Retornando o nome de usuario quando logado
+//TESTE BRANCH
+
+
+//Arumando sistema de login
+//Do Curso do Git
